a29f7e0e1870e277d1aaaf9fd60656f2334beffd
ReeceWBreckon <br12345>
2018-04-05 14:36:02 +0100

    Nearby choices now has the 8 options. Distance bar added so the user can look further for an activity.

--- app/src/main/java/fyp/p4072699/holidayplanner/NearHolidayChoiceActivity.java
+++ app/src/main/java/fyp/p4072699/holidayplanner/NearHolidayChoiceActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class NearHolidayChoiceActivity extends AppCompatActivity implements View.OnClickListener {
-    private ImageButton rest;
+    private ImageButton rest, amuse, aquarium, casino, museum, bar, zoo;
+    private Button retur;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,21 +17,71 @@ public class NearHolidayChoiceActivity extends AppCompatActivity implements View
         setContentView(R.layout.activity_near_holiday_choice);
         setTitle(R.string.near_holiday);
 
+        //Connect to the display
         rest = findViewById(R.id.imageButton_rest);
+        amuse = findViewById(R.id.imageButton_amusement);
+        aquarium = findViewById(R.id.imageButton_aquarium);
+        casino = findViewById(R.id.imageButton_casino);
+        museum = findViewById(R.id.imageButton_museum);
+        bar = findViewById(R.id.imageButton_bar);
+        zoo = findViewById(R.id.imageButton_zoo);
+        retur = findViewById(R.id.button_return);
 
+        //Set the click listeners
         rest.setOnClickListener(this);
+        amuse.setOnClickListener(this);
+        aquarium.setOnClickListener(this);
+        casino.setOnClickListener(this);
+        museum.setOnClickListener(this);
+        bar.setOnClickListener(this);
+        zoo.setOnClickListener(this);
+        retur.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        Intent i = new Intent(NearHolidayChoiceActivity.this, NearHolidayActivity.class);
-        String c = getIntent().getStringExtra("coords");
+        String type = null, title = null;
         switch (view.getId()) {
             case R.id.imageButton_rest:
-                startActivity(i.putExtra("type", "restaurant")
-                        .putExtra("coords", c)
-                        .putExtra("title", "Nearby Restaurants"));
+                type = "restaurant";
+                title = "Nearby Restaurants";
+                break;
+            case R.id.imageButton_amusement:
+                type = "amusement_park";
+                title = "Nearby Amusement Parks";
+                break;
+            case R.id.imageButton_aquarium:
+                type = "aquarium";
+                title = "Nearby Aquariums";
+                break;
+            case R.id.imageButton_casino:
+                type = "casino";
+                title = "Nearby Casinos";
+                break;
+            case R.id.imageButton_museum:
+                type = "museum";
+                title = "Nearby Museums";
+                break;
+            case R.id.imageButton_bar:
+                type = "bar";
+                title = "Nearby Bars";
+                break;
+            case R.id.imageButton_zoo:
+                type = "zoo";
+                title = "Nearby Zoos";
+                break;
+            case R.id.button_return:
+                startActivity(new Intent(NearHolidayChoiceActivity.this, MyHolidaysActivity.class));
                 break;
         }
+        startAct(type, title);
+    }
+
+    public void startAct(String t, String title) {
+        String c = getIntent().getStringExtra("coords");
+        Intent i = new Intent(NearHolidayChoiceActivity.this, NearHolidayActivity.class);
+        startActivity(i.putExtra("type", t)
+                .putExtra("coords", c)
+                .putExtra("title", title));
     }
 }
