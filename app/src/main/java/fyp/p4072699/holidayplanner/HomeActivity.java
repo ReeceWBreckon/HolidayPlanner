70ddda3c5c4531943eb97634b7e320bfe645d9c7
ReeceWBreckon <br12345>
2018-04-05 15:32:18 +0100

    Added a navigation bar to the home screen.

--- app/src/main/java/fyp/p4072699/holidayplanner/HomeActivity.java
+++ app/src/main/java/fyp/p4072699/holidayplanner/HomeActivity.java
@@ -1,16 +1,22 @@
 package fyp.p4072699.holidayplanner;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 
-public class HomeActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button addHoliday, myHolidays, signOut, destinations, profile;
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
+    private Button addHoliday, myHolidays, destinations;
     private FirebaseAuth auth;
+    private DrawerLayout draw;
+    private NavigationView nav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,17 +27,16 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
         //Connect to the display
         addHoliday = findViewById(R.id.button_addholiday);
         myHolidays = findViewById(R.id.button_viewholidays);
-        signOut = findViewById(R.id.button_signout);
         destinations = findViewById(R.id.button_destinations);
-        profile = findViewById(R.id.button_profile);
         auth = FirebaseAuth.getInstance();
+        draw = findViewById(R.id.drawer_layout);
+        nav = findViewById(R.id.nav_bar);
 
         //Add the click listeners
-        profile.setOnClickListener(this);
         destinations.setOnClickListener(this);
         addHoliday.setOnClickListener(this);
         myHolidays.setOnClickListener(this);
-        signOut.setOnClickListener(this);
+        nav.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -43,16 +48,35 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button_viewholidays:
                 startActivity(new Intent(HomeActivity.this, MyHolidaysActivity.class));
                 break;
-            case R.id.button_signout:
-                auth.signOut();
-                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
-                break;
             case R.id.button_destinations:
                 startActivity(new Intent(HomeActivity.this, DestinationsActivity.class));
                 break;
-            case R.id.button_profile:
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        item.setChecked(true);
+        draw.closeDrawers();
+        String s = String.valueOf(item.getTitle());
+        switch (s) {
+            case "Home":
+
+                break;
+            case "My Profile":
                 startActivity(new Intent(HomeActivity.this, ProfileActivity.class));
                 break;
+            case "My Holidays":
+                startActivity(new Intent(HomeActivity.this, MyHolidaysActivity.class));
+                break;
+            case "Destinations":
+                startActivity(new Intent(HomeActivity.this, DestinationsActivity.class));
+                break;
+            case "Sign Out":
+                auth.signOut();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+                break;
         }
+        return false;
     }
 }
